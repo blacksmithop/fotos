47d98f4c96d9912a9cfec730b8d0ee98dfa9ff9c
ambu <60320192+blacksmithop@users.noreply.github.com>
2020-04-07 19:42:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import os,os.path
 from random import randrange
 DIR = './images'
 foto_count = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/craw/', methods=['GET'])
 def respond():
     return jsonify({
-    "url" : f"https://github.com/blacksmithop/fotos/blob/master/images/{randrange(foto_count)}.jpg"
+    "url" : f"https://raw.githubusercontent.com/blacksmithop/fotos/master/images/{randrange(1,foto_count)}.jpg"
     })
 # A welcome message to test our server
 @app.route('/')
