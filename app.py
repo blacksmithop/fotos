17117dfb4ed294bb622f1e058fe7d6a57d055e23
ambu <60320192+blacksmithop@users.noreply.github.com>
2020-04-08 15:08:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def respond():
 def index():
      return render_template("index.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+    
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
